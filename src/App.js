2a060262aa1bd8b2047ddcebebb377ad1e8a4240
Zhihare <128489441+Zhihare@users.noreply.github.com>
2024-02-13 23:51:47 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { lazy, Suspense } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage'));
+const FavoritePage = lazy(() => import('./pages/FavoritePage'));
+
+
+function App() {
+  return (
+    <>
+      <Suspense >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route
+              path="/catalog"
+              element={<CatalogPage />}
+            />
+            <Route
+              path="/favorites"
+              element={<FavoritePage />}
+            />
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
---
+++ src/redax/store.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+
+const rootReducer = combineReducers({
+	// auth: persistReducer(authConfig, authReducer),
+});
+export const store = configureStore({
+	reducer: rootReducer,
+
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export const persistor = persistStore(store);
