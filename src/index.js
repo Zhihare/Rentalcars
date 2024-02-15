1703e266d9a999885ecb404e3732c593dc4ae070
Zhihare <128489441+Zhihare@users.noreply.github.com>
2024-02-16 00:07:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { store, persistor } from './redax/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redax/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 console.log(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
